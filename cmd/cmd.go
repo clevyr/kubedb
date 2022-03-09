a48c8c6861f6488e0bd69d020cbe9ab58bae29da
Gabe Cook <gabe565@gmail.com>
2022-03-09 00:32:04 -0600

    :sparkles: Add support for log-level and log-format

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -4,6 +4,7 @@ import (
 	"github.com/clevyr/kubedb/cmd/dump"
 	"github.com/clevyr/kubedb/cmd/exec"
 	"github.com/clevyr/kubedb/cmd/restore"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"k8s.io/client-go/util/homedir"
 	"path/filepath"
@@ -24,7 +25,12 @@ func Execute() error {
 	return Command.Execute()
 }
 
+const DefaultLogLevel = "info"
+const DefaultLogFormat = "text"
+
 func init() {
+	cobra.OnInitialize(initLog)
+
 	var kubeconfigDefault string
 	if home := homedir.HomeDir(); home != "" {
 		kubeconfigDefault = filepath.Join(home, ".kube", "config")
@@ -32,6 +38,9 @@ func init() {
 	Command.PersistentFlags().String("kubeconfig", kubeconfigDefault, "absolute path to the kubeconfig file")
 	Command.PersistentFlags().StringP("namespace", "n", "", "the namespace scope for this CLI request")
 	Command.PersistentFlags().String("grammar", "", "database grammar. one of: (postgres|mariadb)")
+
+	Command.PersistentFlags().String("log-level", DefaultLogLevel, "log level. one of: trace|debug|info|warning|error|fatal|panic")
+	Command.PersistentFlags().String("log-format", DefaultLogFormat, "log formatter. one of: text|json")
 
 	Command.PersistentFlags().Bool("github-actions", false, "Enables GitHub Actions log output")
 	_ = Command.PersistentFlags().MarkHidden("github-actions")
@@ -41,6 +50,28 @@ func init() {
 		dump.Command,
 		restore.Command,
 	)
+}
+
+func initLog() {
+	logLevel, _ := Command.PersistentFlags().GetString("log-level")
+	parsedLevel, err := log.ParseLevel(logLevel)
+	if err != nil {
+		log.WithField("log-level", logLevel).Warn("invalid log level. defaulting to info.")
+		_ = Command.PersistentFlags().Set("log-level", DefaultLogLevel)
+		parsedLevel = log.InfoLevel
+	}
+	log.SetLevel(parsedLevel)
+
+	logFormat, _ := Command.PersistentFlags().GetString("log-format")
+	switch logFormat {
+	case "text", "txt", "t":
+		log.SetFormatter(&log.TextFormatter{})
+	case "json", "j":
+		log.SetFormatter(&log.JSONFormatter{})
+	default:
+		log.WithField("log-format", logFormat).Warn("invalid log formatter. defaulting to text.")
+		_ = Command.PersistentFlags().Set("log-format", DefaultLogFormat)
+	}
 }
 
 func buildVersion() string {
