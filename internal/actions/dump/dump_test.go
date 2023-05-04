e620ba76e49cff41a089334b191956d9d6d17be4
Gabe Cook <gabe565@gmail.com>
2023-05-04 17:24:47 -0500

    :white_check_mark: (dump): Fix tests with remote-gzip flag

--- internal/actions/dump/dump_test.go
+++ internal/actions/dump/dump_test.go
@@ -24,22 +24,27 @@ func Test_buildCommand(t *testing.T) {
 	}{
 		{
 			"postgres-gzip",
-			args{Dump{Dump: config.Dump{Global: config.Global{Dialect: dialect.Postgres{}, Database: "d", Username: "u"}}}},
+			args{Dump{Dump: config.Dump{Global: config.Global{Dialect: dialect.Postgres{}, Database: "d", Username: "u", RemoteGzip: true}}}},
 			command.NewBuilder(pgpassword, "pg_dump", "--host=127.0.0.1", "--username=u", "--dbname=d", "--verbose", command.Pipe, "gzip", "--force"),
 		},
 		{
+			"postgres-gzip-no-compression",
+			args{Dump{Dump: config.Dump{Global: config.Global{Dialect: dialect.Postgres{}, Database: "d", Username: "u"}}}},
+			command.NewBuilder(pgpassword, "pg_dump", "--host=127.0.0.1", "--username=u", "--dbname=d", "--verbose"),
+		},
+		{
 			"postgres-plain",
-			args{Dump{Dump: config.Dump{Files: config.Files{Format: sqlformat.Plain}, Global: config.Global{Dialect: dialect.Postgres{}, Database: "d", Username: "u"}}}},
+			args{Dump{Dump: config.Dump{Files: config.Files{Format: sqlformat.Plain}, Global: config.Global{Dialect: dialect.Postgres{}, Database: "d", Username: "u", RemoteGzip: true}}}},
 			command.NewBuilder(pgpassword, "pg_dump", "--host=127.0.0.1", "--username=u", "--dbname=d", "--verbose", command.Pipe, "gzip", "--force"),
 		},
 		{
 			"postgres-custom",
-			args{Dump{Dump: config.Dump{Files: config.Files{Format: sqlformat.Custom}, Global: config.Global{Dialect: dialect.Postgres{}, Database: "d", Username: "u"}}}},
+			args{Dump{Dump: config.Dump{Files: config.Files{Format: sqlformat.Custom}, Global: config.Global{Dialect: dialect.Postgres{}, Database: "d", Username: "u", RemoteGzip: true}}}},
 			command.NewBuilder(pgpassword, "pg_dump", "--host=127.0.0.1", "--username=u", "--dbname=d", "--format=c", "--verbose"),
 		},
 		{
 			"mariadb-gzip",
-			args{Dump{Dump: config.Dump{Files: config.Files{Format: sqlformat.Gzip}, Global: config.Global{Dialect: dialect.MariaDB{}, Database: "d", Username: "u"}}}},
+			args{Dump{Dump: config.Dump{Files: config.Files{Format: sqlformat.Gzip}, Global: config.Global{Dialect: dialect.MariaDB{}, Database: "d", Username: "u", RemoteGzip: true}}}},
 			command.NewBuilder(mysql_pwd, "mysqldump", "--host=127.0.0.1", "--user=u", "d", "--verbose", command.Pipe, "gzip", "--force"),
 		},
 	}
