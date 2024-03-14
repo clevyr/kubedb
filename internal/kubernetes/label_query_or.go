261b18f5a55d16d504128a0577e91480d9c808c1
Gabe Cook <gabe565@gmail.com>
2024-03-14 03:53:28 -0500

    chore(kubernetes): Add `LabelQueryOr` interface

---
+++ internal/kubernetes/label_query_or.go
@@ -0,0 +1,28 @@
+package kubernetes
+
+import (
+	v1 "k8s.io/api/core/v1"
+)
+
+type LabelQueryOr []LabelQuery
+
+func (queries LabelQueryOr) Matches(pod v1.Pod) bool {
+	for _, query := range queries {
+		if query.Matches(pod) {
+			return true
+		}
+	}
+	return false
+}
+
+func (queries LabelQueryOr) FindPods(pods []v1.Pod) []v1.Pod {
+	matched := make([]v1.Pod, 0, len(pods))
+
+	for _, pod := range pods {
+		if queries.Matches(pod) {
+			matched = append(matched, pod)
+		}
+	}
+
+	return matched
+}
