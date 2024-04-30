4212473ef365b216e7b52450ea7865856707bee3
Gabe Cook <gabe565@gmail.com>
2024-04-30 18:47:11 -0500

    chore(tui): Make lipgloss cache the current background color early
    Attempting to use stdin after remotecommand.Executor results in an EOF,
    so this value must be cached early to allow KubeDB to print tables post-exec.

---
+++ internal/tui/tui.go
@@ -0,0 +1,11 @@
+package tui
+
+import "github.com/charmbracelet/lipgloss"
+
+//nolint:gochecknoinits
+func init() {
+	// Make lipgloss cache the current background color.
+	// Attempting to use stdin after remotecommand.Executor results in an EOF,
+	// so this value must be cached early to allow KubeDB to print tables post-exec.
+	_ = lipgloss.HasDarkBackground()
+}
