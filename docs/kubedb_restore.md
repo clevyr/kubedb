829520a95d98eebd4ae832fb0120bc2d5a0d5fbc
Gabe Cook <gabe565@gmail.com>
2024-04-30 00:57:44 -0500

    chore(config): Optimize logger and add values to explicitly toggle color

--- docs/kubedb_restore.md
+++ docs/kubedb_restore.md
@@ -47,7 +47,7 @@ kubedb restore filename [flags]
       --dialect string                 Database dialect. One of (postgres|mariadb|mongodb) (default discovered)
       --healthchecks-ping-url string   Notification handler URL
       --kubeconfig string              Paths to the kubeconfig file (default "$HOME/.kube/config")
-      --log-format string              Log formatter. One of (text|json) (default "text")
+      --log-format string              Log formatter. One of (auto|color|plain|json) (default "auto")
       --log-level string               Log level. One of (trace|debug|info|warn|error|fatal|panic) (default "info")
   -n, --namespace string               Kubernetes namespace
       --pod string                     Perform detection from a pod instead of searching the namespace
--- internal/config/log.go
+++ internal/config/log.go
@@ -2,60 +2,73 @@ package config
 
 import (
 	"fmt"
+	"io"
 	"os"
 
-	"github.com/charmbracelet/lipgloss"
-	"github.com/clevyr/kubedb/internal/consts"
+	"github.com/fatih/color"
 	"github.com/mattn/go-isatty"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
-func InitLog(cmd *cobra.Command) {
-	logLevel := viper.GetString(consts.LogLevelKey)
-	parsedLevel, err := zerolog.ParseLevel(logLevel)
-	if err != nil {
-		if logLevel == "warning" {
+func LogLevel(level string) zerolog.Level {
+	parsedLevel, err := zerolog.ParseLevel(level)
+	if err != nil || parsedLevel == zerolog.NoLevel {
+		if level == "warning" {
 			parsedLevel = zerolog.WarnLevel
 		} else {
-			log.Warn().Str("level", logLevel).Msg("invalid log level. defaulting to info.")
-			viper.Set(consts.LogLevelKey, zerolog.InfoLevel.String())
+			log.Warn().Str("value", level).Msg("invalid log level. defaulting to info.")
 			parsedLevel = zerolog.InfoLevel
 		}
 	}
-	zerolog.SetGlobalLevel(parsedLevel)
+	return parsedLevel
+}
 
-	logFormat := viper.GetString(consts.LogFormatKey)
-	switch logFormat {
-	case "text", "txt", "t":
-		var useColor bool
-		baseFormatter := func(i interface{}) string {
-			return fmt.Sprintf("%-45s", i)
-		}
-		formatter := baseFormatter
-		errOut := cmd.ErrOrStderr()
-		if w, ok := errOut.(*os.File); ok {
-			useColor = isatty.IsTerminal(w.Fd())
-			if useColor {
-				boldStyle := lipgloss.NewStyle().Bold(true)
-				formatter = func(i interface{}) string {
-					return boldStyle.Render(baseFormatter(i))
-				}
-			}
-		}
-
-		log.Logger = log.Output(zerolog.ConsoleWriter{
-			Out:           errOut,
-			NoColor:       !useColor,
-			FormatMessage: formatter,
-		})
+func LogFormat(out io.Writer, format string) io.Writer {
+	switch format {
 	case "json", "j":
-		// default
+		return out
 	default:
-		log.Warn().Str("format", logFormat).Msg("invalid log formatter. defaulting to text.")
-		viper.Set(consts.LogFormatKey, "text")
-		InitLog(cmd)
+		var useColor bool
+		sprintf := fmt.Sprintf
+		switch format {
+		case "auto", "a", "text", "txt", "t":
+			if w, ok := out.(*os.File); ok {
+				useColor = isatty.IsTerminal(w.Fd())
+			}
+			if !useColor {
+				break
+			}
+			fallthrough
+		case "color", "c":
+			useColor = true
+			sprintf = color.New(color.Bold).Sprintf
+		case "plain", "p":
+		default:
+			log.Warn().Str("value", format).Msg("invalid log formatter. defaulting to auto.")
+		}
+
+		return zerolog.ConsoleWriter{
+			Out:     out,
+			NoColor: !useColor,
+			FormatMessage: func(i interface{}) string {
+				return sprintf("%-45s", i)
+			},
+		}
 	}
+}
+
+func InitLog(cmd *cobra.Command) {
+	level, err := cmd.Flags().GetString("log-level")
+	if err != nil {
+		panic(err)
+	}
+	zerolog.SetGlobalLevel(LogLevel(level))
+
+	format, err := cmd.Flags().GetString("log-format")
+	if err != nil {
+		panic(err)
+	}
+	log.Logger = log.Output(LogFormat(cmd.ErrOrStderr(), format))
 }
