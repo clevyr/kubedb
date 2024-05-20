57c26ec7a0d94c21e6754d5a07fec8ac0eabab4e
Gabe Cook <gabe565@gmail.com>
2024-05-20 03:21:31 -0500

    test(lint): Fix golangci-lint v1.58.0 warnings

--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -2,8 +2,7 @@ package tui
 
 import "github.com/charmbracelet/lipgloss"
 
-//nolint:gochecknoinits
-func init() {
+func init() { //nolint:all
 	// Make lipgloss cache the current background color.
 	// Attempting to use stdin after remotecommand.Executor results in an EOF,
 	// so this value must be cached early to allow KubeDB to print tables post-exec.
