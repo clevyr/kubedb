1472a8aabbb9193e345b15964a7ce9e0dad3ad7c
Gabe Cook <gabe565@gmail.com>
2024-08-01 14:42:09 -0500

    chore(flags): Change default spinner to `dots14`

--- internal/config/flags/generic.go
+++ internal/config/flags/generic.go
@@ -6,7 +6,7 @@ import (
 	"github.com/spf13/viper"
 )
 
-const DefaultSpinner = "sand"
+const DefaultSpinner = "dots14"
 
 func Spinner(cmd *cobra.Command, p *string) {
 	cmd.Flags().StringVar(p, consts.SpinnerFlag, DefaultSpinner, "Spinner from https://jsfiddle.net/sindresorhus/2eLtsbey/embedded/result/")
