16436c6a2dcb19718a7e3feac0f18ef36814c046
Gabe Cook <gabe565@gmail.com>
2024-09-19 14:54:23 -0500

    chore: Simplify installation of completions and man pages

--- internal/generate/completions/main.go
+++ internal/generate/completions/main.go
@@ -9,22 +9,42 @@ import (
 	"github.com/clevyr/kubedb/cmd"
 )
 
+const (
+	shellBash = "bash"
+	shellZsh  = "zsh"
+	shellFish = "fish"
+)
+
 func main() {
 	rootCmd := cmd.NewCommand()
 	var buf bytes.Buffer
 	rootCmd.SetOut(&buf)
 
-	if err := os.MkdirAll("completions", 0o777); err != nil {
+	if err := os.RemoveAll("completions"); err != nil {
 		panic(err)
 	}
 
-	for _, shell := range []string{"bash", "zsh", "fish"} {
+	for _, shell := range []string{shellBash, shellZsh, shellFish} {
 		rootCmd.SetArgs([]string{"completion", shell})
 		if err := rootCmd.Execute(); err != nil {
 			panic(err)
 		}
 
-		f, err := os.Create(filepath.Join("completions", "kubedb."+shell))
+		path := filepath.Join("completions", shell)
+		if err := os.MkdirAll(path, 0o777); err != nil {
+			panic(err)
+		}
+
+		switch shell {
+		case shellBash:
+			path = filepath.Join(path, rootCmd.Name())
+		case shellZsh:
+			path = filepath.Join(path, "_"+rootCmd.Name())
+		case shellFish:
+			path = filepath.Join(path, rootCmd.Name()+".fish")
+		}
+
+		f, err := os.Create(path)
 		if err != nil {
 			panic(err)
 		}
