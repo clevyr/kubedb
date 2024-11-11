f0986b8975f076148b7e5c1c0d51f52174c697f3
Gabe Cook <gabe565@gmail.com>
2024-11-11 17:36:50 -0600

    chore(util): Change network policy failure log from error to warn level

--- internal/util/cmd_setup.go
+++ internal/util/cmd_setup.go
@@ -391,7 +391,7 @@ func createJob(ctx context.Context, conf *config.Global, actionName string) erro
 
 		nsLog.Debug("Creating network policy")
 		if _, err := conf.Client.NetworkPolicies().Create(ctx, &policy, metav1.CreateOptions{}); err != nil {
-			nsLog.Error("Failed to create network policy", "error", err)
+			nsLog.Warn("Failed to create network policy", "error", err)
 			viper.Set(consts.CreateNetworkPolicyKey, "false")
 		}
 	}
